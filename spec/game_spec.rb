114980ffca743497c02f0b2be3d76390f096bfea
turbocep <162988238+turbocep@users.noreply.github.com>
2024-11-28 14:05:12 +0100

    refactor(game_spec): DRY up tests with #let

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -16,36 +16,32 @@ describe Game do
     end
   end
   describe '#add' do
+    let(:game) { described_class.new }
     context 'when board is empty' do
       context 'when piece is added to column 1' do
         it 'gets added to bottom' do
-          board = Game.new
-          board.add(1, 'X')
-          expect(board.board[35]).to eql 'X'
+          game.add(1, 'X')
+          expect(game.board[35]).to eql 'X'
         end
       end
       context 'when piece is added to column 7' do
         it 'gets added to bottom' do
-          board = Game.new
-          board.add(7, 'X')
-          expect(board.board[41]).to eql('X')
+          game.add(7, 'X')
+          expect(game.board[41]).to eql('X')
         end
         it 'increases number of pieces by 1' do
-          board = Game.new
-          expect {board.add(7, 'X')}.to change{board.board.count('X')}.from(0).to(1)
+          expect {game.add(7, 'X')}.to change{game.board.count('X')}.from(0).to(1)
         end
         it 'decreases number of empty elements by 1' do
-          board = Game.new
-          expect {board.add(7, 'X')}.to change {board.board.count('_')}.from(42).to(41)
+          expect {game.add(7, 'X')}.to change {game.board.count('_')}.from(42).to(41)
         end
       end
     end
     context 'when symbol is added in non-empty column' do
       it 'gets added on top' do
-        board = Game.new
-        board.add(3, 'X')
-        board.add(3, 'O')
-        expect(board.board[30]).to eql('O')
+        game.add(3, 'X')
+        game.add(3, 'O')
+        expect(game.board[30]).to eql('O')
       end
     end
   end
