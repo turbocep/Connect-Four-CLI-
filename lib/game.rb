b67f601f008d0118c3653ff090d46a9af8b78bcc
turbocep <162988238+turbocep@users.noreply.github.com>
2024-11-28 13:24:48 +0100

    feat(Game): create and visually test #render
    The render method was not tested with RSpec as it
    was deemed too time-consuming to do so. It was
    tested visually. Tests may be written for it in
    the future, but not necessarily, as it is easy
    to visually test the method while running.

--- lib/game.rb
+++ lib/game.rb
@@ -5,9 +5,23 @@ class Game
   def initialize(columns = 7, rows = 6, players = [Player.new, Player.new])
     @columns = columns
     @rows = rows
-    @board = Array.new(columns * rows, ' ')
+    @board = Array.new(columns * rows, '_')
     @players = players
   end
+
+  def render
+    (1..@columns).each { |c| print " #{c}"}
+    print " \n"
+    @board.each_with_index do | element, i |
+      if (i + 1) % @columns > 0
+        print "|#{element}"
+      else
+        print "|#{element}|\n"
+      end
+    end
+  end
 end
+
+Game.new.render
 
 
