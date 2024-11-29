4cd45692e98019512dc567ee4f9165da5268d1cf
turbocep <162988238+turbocep@users.noreply.github.com>
2024-11-29 13:19:21 +0100

    feat(Player): create and test user input methods

--- lib/player.rb
+++ lib/player.rb
@@ -4,4 +4,28 @@ class Player
     @name = name
     @type = type
   end
+
+  def get_move(valid_moves)
+    #TODO(Turbo): Expand options for Player type.
+    case type
+    when :user then user_input(valid_moves)
+    end
+  end
+
+  # User Input method: I could probably do this with Lambdas.
+  def user_input(valid_moves)
+    loop do
+      puts "#{name}, enter your move:"
+      move = gets.chomp.to_i
+      return move if valid_move?(move, valid_moves)
+      puts "Invalid input #{name}, try again."
+    end
+  end
+
+  def valid_move?(move, valid_moves)
+    valid_moves.include?(move)
+  end
 end
+
+player = Player.new
+p player.get_move([1])
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -25,4 +25,56 @@ describe Player do
       end
     end
   end
+  describe '#valid_move?' do
+    let(:player) {Player.new}
+    context 'when valid move' do
+      it 'returns true' do
+        valid_moves = [1, 2, 3, 4, 5, 6, 7]
+        move = 4
+        expect(player.valid_move?(move, valid_moves)).to be true
+      end
+      it 'returns true' do
+        valid_moves = [1, 2, 3, 4, 5, 6, 7]
+        move = 1
+        expect(player.valid_move?(move, valid_moves)).to be true
+      end
+      it 'returns true' do
+        valid_moves = [1, 2, 3, 4, 5, 6, 7]
+        move = 7
+        expect(player.valid_move?(move, valid_moves)).to be true
+      end
+      it 'returns true' do
+        valid_moves = [6]
+        move = 6
+        expect(player.valid_move?(move, valid_moves)).to be true
+      end
+    end
+    context 'when invalid move' do
+      it 'returns false' do
+        valid_moves = [1, 2, 3, 4, 5, 6, 7]
+        move = 8
+        expect(player.valid_move?(move, valid_moves)).to be false
+      end
+      it 'returns false' do
+        valid_moves = [1, 2, 3, 4, 5, 6, 7]
+        move = 0
+        expect(player.valid_move?(move, valid_moves)).to be false
+      end
+      it 'returns false' do
+        valid_moves = [1, 2, 3, 4, 5, 6, 7]
+        move = -3
+        expect(player.valid_move?(move, valid_moves)).to be false
+      end
+      it 'returns false' do
+        valid_moves = [4, 5, 6, 7]
+        move = 3
+        expect(player.valid_move?(move, valid_moves)).to be false
+      end
+      it 'returns false' do
+        valid_moves = []
+        move = 6
+        expect(player.valid_move?(move, valid_moves)).to be false
+      end
+    end
+  end
 end
